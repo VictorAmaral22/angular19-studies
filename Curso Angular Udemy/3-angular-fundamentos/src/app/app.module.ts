6ea68df923b870fdb1075148a33987996e0dd64d
victoramaral22 <victortavamaral@gmail.com>
2025-02-27 20:13:47 -0300

    wow

--- Curso Angular Udemy/3-angular-fundamentos/src/app/app.module.ts
+++ Curso Angular Udemy/3-angular-fundamentos/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponentComponent } from './card-component/card-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ Curso Angular Udemy/3-angular-fundamentos/src/app/card-component/card-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-component',
+  standalone: false,
+  templateUrl: './card-component.component.html',
+  styleUrl: './card-component.component.css'
+})
+export class CardComponentComponent {
+
+}
